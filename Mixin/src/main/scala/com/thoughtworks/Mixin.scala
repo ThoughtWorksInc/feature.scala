ec7182b3ba215c48573615a41f115577bc9a433f
Yang Bo <atryyang@thoughtworks.com>
2017-05-07 19:16:32 +0800

    Hide LowPriorityMixin from Scaladoc

--- Mixin/src/main/scala/com/thoughtworks/Mixin.scala
+++ Mixin/src/main/scala/com/thoughtworks/Mixin.scala
@@ -5,15 +5,14 @@ trait Mixin[SuperTypes <: HList] {
   type Out
 }
 
-trait LowPriorityMixin {
-
+private[thoughtworks] trait LowPriorityMixin { this: Mixin.type =>
   implicit def hconsMixin[Head, TailOut, Tail <: HList](
       implicit tailMixin: Mixin.Aux[Tail, TailOut]): Mixin.Aux[Head :: Tail, Head with TailOut] =
     new Mixin[Head :: Tail] {
       override type Out = Head with TailOut
     }
-
 }
+
 object Mixin extends LowPriorityMixin {
   type Aux[SuperTypes <: HList, Out0] = Mixin[SuperTypes] {
     type Out = Out0
@@ -22,5 +21,4 @@ object Mixin extends LowPriorityMixin {
   implicit def singletonMixin[Head]: Mixin.Aux[Head :: HNil, Head] = new Mixin[Head :: HNil] {
     override type Out = Head
   }
-
 }
