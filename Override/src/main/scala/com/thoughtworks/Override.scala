390423801a2f66001f663535dd476f3267b17a00
Yang Bo <atryyang@thoughtworks.com>
2017-05-10 00:23:23 +0800

    Automatically generate abstract types and injected abstract methods

--- Override/src/main/scala/com/thoughtworks/Override.scala
+++ Override/src/main/scala/com/thoughtworks/Override.scala
@@ -1,9 +1,14 @@
 package com.thoughtworks
+import com.thoughtworks.DelayMacros.DelayTreeCreator
 import macrocompat.bundle
 import shapeless._
-import scala.language.experimental.macros
 
+import scala.annotation.StaticAnnotation
+import scala.collection.immutable.Queue
+import scala.language.dynamics
+import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
+import scala.util.control.NonFatal
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
@@ -12,14 +17,22 @@ final class Override[Vals, Result](val newInstanceRecord: Vals => Result) extend
 
 object Override {
 
+  final class inject extends StaticAnnotation
+  final class mixinLowerBounds extends StaticAnnotation
+
   def apply[Vals, Result](implicit constructor: Override[Vals, Result]): Override[Vals, Result] = constructor
 
   implicit def materialize[Vals, Result]: Override[Vals, Result] = macro Macros.materialize[Vals, Result]
 
   @bundle
-  final class Macros(val c: whitebox.Context) extends CaseClassMacros with SingletonTypeUtils {
+  private[Override] final class Macros(val c: whitebox.Context)
+      extends CaseClassMacros
+      with SingletonTypeUtils
+      with DelayMacros {
     import c.universe._
 
+    private val injectType = typeOf[inject]
+    private val mixinLowerBoundsType = typeOf[mixinLowerBounds]
     private def demixin(t: Type): Stream[Type] = {
       t.dealias match {
         case RefinedType(superTypes, refinedScope) if refinedScope.isEmpty =>
@@ -29,24 +42,119 @@ object Override {
       }
     }
 
-    def materialize[Vals: WeakTypeTag, Result: WeakTypeTag]: Tree = {
-      val valsType = weakTypeOf[Vals]
-      val pattern = unpackHListTpe(valsType).foldRight[Tree](pq"_root_.shapeless.HNil") { (field, accumulator) =>
-        val FieldType(SingletonSymbolType(k), v) = field
-        pq"_root_.shapeless.::(${TermName(k)}, $accumulator)"
-      }
-      val argumentHListName = TermName(c.freshName("argumentHList"))
-      val mixinType = weakTypeOf[Result]
-      val superTrees = for (superType <- demixin(mixinType)) yield {
-        tq"$superType"
-      }
-      q"""
+    def materialize[Vals: WeakTypeTag, Result: WeakTypeTag]: Tree =
+      try {
+        val valsType = weakTypeOf[Vals]
+        val pattern = unpackHListTpe(valsType).foldRight[Tree](pq"_root_.shapeless.HNil") { (field, accumulator) =>
+          val FieldType(SingletonSymbolType(k), v) = field
+          pq"_root_.shapeless.::(${TermName(k)}, $accumulator)"
+        }
+        val argumentHListName = TermName(c.freshName("argumentHList"))
+        val mixinType = weakTypeOf[Result]
+
+        val injects = for {
+          baseClass <- mixinType.baseClasses
+          member <- baseClass.info.decls
+          if member.isMethod && {
+            c.internal.initialize(member)
+            member.annotations.exists { a =>
+              a.tree.tpe <:< injectType
+            }
+          }
+        } yield {
+          delayValOrDef(new DelayTreeCreator {
+            override def apply(c: whitebox.Context): c.universe.Tree =
+              try {
+                import c.universe._
+
+                val memberSymbol = member.asInstanceOf[Symbol].asMethod
+                val methodName = memberSymbol.name.toTermName
+
+//                def  narrowSuperType = internal.superType(internal.thisType(c.internal.enclosingOwner),
+//                                                         internal.thisType(memberSymbol.owner))
+//
+//                val methodType = memberSymbol.infoIn(narrowSuperType) // TODO: need Type To Tree conversion for better this.type
+                val methodType = memberSymbol.info
+                val result = if (memberSymbol.isVar) {
+                  q"override var $methodName = _root_.scala.Predef.implicitly"
+                } else if (memberSymbol.isVal) {
+                  q"override val $methodName = _root_.scala.Predef.implicitly"
+                } else {
+                  val argumentTrees = methodType.paramLists.map(_.map { argumentSymbol =>
+                    if (argumentSymbol.asTerm.isImplicit) {
+                      q"implicit val ${argumentSymbol.name.toTermName}: ${argumentSymbol.info}"
+                    } else {
+                      q"val ${argumentSymbol.name.toTermName}: ${argumentSymbol.info}"
+                    }
+                  })
+                  q"override def $methodName[..${methodType.typeArgs}](...$argumentTrees) = _root_.scala.Predef.implicitly"
+                }
+                result
+              } catch {
+                case NonFatal(e) =>
+                  c.warning(c.enclosingPosition, show(e))
+                  throw e
+              }
+          })
+        }
+        val overridenTypes =
+          (for {
+            baseClass <- mixinType.baseClasses
+            member <- baseClass.info.decls
+            if member.isType
+          } yield member)
+            .groupBy(_.name.toString)
+            .withFilter {
+              _._2.forall {
+                _.info match {
+                  case TypeBounds(_, _) => true
+                  case _ => false
+                }
+              }
+            }
+            .map {
+              case (name, members) =>
+                delayType(new DelayTreeCreator {
+                  override def apply(c: whitebox.Context): c.universe.Tree =
+                    try {
+                      import c.universe._
+
+                      val lowerBounds = members.map { member =>
+                        val memberSymbol = member.asInstanceOf[Symbol]
+                        val narrowSuperType = internal.superType(internal.thisType(c.internal.enclosingOwner),
+                                                                 internal.thisType(memberSymbol.owner))
+                        val TypeBounds(_, lowerBound) = memberSymbol.infoIn(narrowSuperType)
+                        tq"$lowerBound"
+                      }
+                      val compoundTypeTree = CompoundTypeTree(Template(lowerBounds.toList, noSelfType, Nil))
+                      q"override type ${TypeName(name)} = $compoundTypeTree"
+                    } catch {
+                      case NonFatal(e) =>
+                        c.warning(c.enclosingPosition, show(e))
+                        throw e
+                    }
+                })
+            }
+        val superTypes: Stream[c.universe.Type] = demixin(mixinType)
+
+        val superTrees = for (superType <- superTypes) yield {
+          tq"$superType"
+        }
+        val result = q"""
         new _root_.com.thoughtworks.Override[$valsType, $mixinType]({$argumentHListName: $valsType =>
           new ..$superTrees {
             override val $pattern = $argumentHListName
+            ..$overridenTypes
+            ..$injects
           }
         })
       """
-    }
+//        c.info(c.enclosingPosition, showCode(result), false)
+        result
+      } catch {
+        case NonFatal(e) =>
+          c.warning(c.enclosingPosition, show(e))
+          throw e
+      }
   }
 }
--- Override/src/test/scala/com/thoughtworks/OverrideSpec.scala
+++ Override/src/test/scala/com/thoughtworks/OverrideSpec.scala
@@ -1,5 +1,6 @@
 package com.thoughtworks
 
+import com.thoughtworks.Override.{inject, mixinLowerBounds}
 import org.scalatest.{FreeSpec, Matchers}
 import shapeless._
 import shapeless.labelled.FieldType
@@ -17,6 +18,20 @@ final class OverrideSpec extends FreeSpec with Matchers {
     a.x should be("x")
     a.y should be("y")
   }
+
+  "xx" in {
+    Override[HNil, AbstractTypeOwner0 with AbstractTypeOwner1]
+    Override[HNil, Global0 with Global1]
+    new Global0 with Global1 {
+      override type Local = super[Global0].LocalApi with super[Global1].LocalApi
+      override def xxx = implicitly
+    }
+  }
+
+//  "yy" in {
+//    Override[HNil, Injected]
+//  }
+
 }
 
 private object OverrideSpec {
@@ -25,6 +40,34 @@ private object OverrideSpec {
     def i: Int = 0
     def x: String
     def y: String
+  }
+
+  trait AbstractTypeOwner0 {
+    type T >: List[Nothing] <: Seq[Any]
+  }
+
+  trait AbstractTypeOwner1 {
+    type T <: Iterable[String]
+  }
+
+  trait Injected {
+    @inject
+    def foo(implicit x: Ordering[Int]): Int =:= Int
+  }
+
+  trait Global0 {
+    trait LocalApi
+
+    type Local <: LocalApi
+
+    @inject
+    def xxx: Override[HNil, Local]
+
+  }
+  trait Global1 {
+    trait LocalApi
+    type Local <: LocalApi
+
   }
 
 }
