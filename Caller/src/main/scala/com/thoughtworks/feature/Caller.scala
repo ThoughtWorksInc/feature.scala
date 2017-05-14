7f5e1c7eacee652314f7becb98d31edbabfcd265
Yang Bo <atryyang@thoughtworks.com>
2017-05-14 23:39:12 +0800

    Move README to Scaladoc

--- Caller/src/main/scala/com/thoughtworks/feature/Caller.scala
+++ Caller/src/main/scala/com/thoughtworks/feature/Caller.scala
@@ -3,6 +3,57 @@ package com.thoughtworks.feature
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox.Context
 
+/** An implicit value that points to the function caller.
+  * 
+  * @example
+  * {{{
+  * libraryDependencies += "com.thoughtworks.feature" %% "caller" % "latest.release"
+  * }}}
+  *
+  * === Getting the caller for logging or something ===
+  * {{{
+  * object Foo{
+  *   def log()(implicit caller: Caller[Any]) = {
+  *     println(caller.value)
+  *   }
+  * }
+  * object Bar{
+  *   Foo.log() // Bar
+  * }
+  * }}}
+  * 
+  * === Restricting who you can be called from ===
+  * {{{
+  * class IKnowWhatImDoing
+  * object Foo{
+  *   def runDangerous()(implicit caller: Caller[IKnowWhatImDoing]) = {
+  *     println(caller.value)
+  *   }
+  * }
+  * object Bar {
+  *   Foo.runDangerous() // compile error
+  * }
+  * object Bar2 extends IKnowWhatImDoing{
+  *   Foo.runDangerous() // ok, prints Bar2
+  * }
+  * }}}
+  *
+  * === Getting calling class or classloader, e.g. for loading resources, without needing to worry about properly setting up and tearing down the Context ClassLoader ===
+  * {{{
+  * object Foo{
+  *   def getResource(path: String)(implicit caller: Caller[_]) = {
+  *     caller.value.getClass.getClassLoader.getResourceAsStream(path)
+  *   }
+  * }
+  * object Bar{
+  *   Foo.getResource("/thing/file.txt") // loads resource from `Bar`s classloader, always
+  * }
+  * }}}
+  * 
+  * @param value The caller instance
+  * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
+  * @author Li Haoyi
+  */
 final class Caller[+A](val value: A) extends AnyVal
 
 object Caller {
