aac0da9f8feb95b48f23876b9d09b314ff28a77a
杨博 (Yang Bo) <pop.atry@gmail.com>
2017-05-16 17:52:02 +0800

    Update Caller.scala

--- Caller/src/main/scala/com/thoughtworks/feature/Caller.scala
+++ Caller/src/main/scala/com/thoughtworks/feature/Caller.scala
@@ -5,6 +5,7 @@ import scala.reflect.macros.whitebox.Context
 
 /** An implicit value that points to the function caller.
   * 
+  * == Usage ==
   * {{{
   * libraryDependencies += "com.thoughtworks.feature" %% "caller" % "latest.release"
   * }}}
