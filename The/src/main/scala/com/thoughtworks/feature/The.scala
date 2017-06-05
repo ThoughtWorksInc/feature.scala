457cd81de15be47cd80218524b4dcb9e0bc1c3b5
Yang Bo <atryyang@thoughtworks.com>
2017-06-05 17:46:57 +0800

    Add the constraint Widen <: Narrow

--- The/src/main/scala/com/thoughtworks/feature/The.scala
+++ The/src/main/scala/com/thoughtworks/feature/The.scala
@@ -1,7 +1,7 @@
 package com.thoughtworks.feature
 import scala.language.higherKinds
 
-final class The[Widen, Narrow] private[The] (val value: Narrow) extends AnyVal
+final class The[Widen, Narrow <: Widen] private[The] (val value: Narrow) extends AnyVal
 
 object The {
   implicit def apply[Widen](implicit t: Widen): The[Widen, t.type] = new The[Widen, t.type](t)
