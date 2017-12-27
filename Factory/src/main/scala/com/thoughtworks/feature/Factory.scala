6094c3f79b27817172627304ed812c895c4ea4e2
Yang Bo <pop.atry@gmail.com>
2017-12-27 14:20:20 +0800

    Create anonymous class name from the enclosing owner

--- Factory/src/main/scala/com/thoughtworks/feature/Factory.scala
+++ Factory/src/main/scala/com/thoughtworks/feature/Factory.scala
@@ -211,7 +211,7 @@ object Factory {
       val linearSymbol = linearOutput.typeSymbol
       val linearThis = internal.thisType(linearSymbol)
 
-      val mixinClassName = TypeName(c.freshName("Anonymous"))
+      val mixinClassName = c.freshName(c.internal.enclosingOwner.name.toTypeName)
 
       def untyper = new Untyper[c.universe.type](c.universe) {
         private def replaceThisValue: PartialFunction[Type, Tree] = {
